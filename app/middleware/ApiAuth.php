a86220bfb9f72cf841677e61ece5c5ad5bacd12e
阿沁 <youziyouzishu@gmail.com>
2025-03-29 14:32:53 +0800

    feat(admin): 添加用户邮箱和手机号字段
    - 在用户禁止访问页面添加邮箱和手机号字段
    - 在用户列表页面添加客户端类型筛选功能
    - 优化用户列表页面的渲染逻辑

--- app/middleware/ApiAuth.php
+++ app/middleware/ApiAuth.php
@@ -34,7 +34,6 @@ class ApiAuth implements MiddlewareInterface
                 // 访问的方法需要登录
                 $request->user_id = JwtToken::getCurrentId();
             }
-
         }
         // 如果是options请求则返回一个空响应，否则继续向洋葱芯穿越，并得到一个响应
         $response = $request->method() == 'OPTIONS' ? response('') : $handler($request);
